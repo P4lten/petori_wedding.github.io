fa9df4474d3754317800333e24dbccafeb53e4da
P4lten <altenbergerp@gmail.com>
2025-11-09 16:18:52 +0100

    icon change and adding header

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-//import Header from './components/Header.js';
+import Header from './components/Header.js';
 
 function App() {
 
 
   return (
     <>
-    
+    <Header/>
       <h1>Petori Hochzeit</h1>
     </>
   )
