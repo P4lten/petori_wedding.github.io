bbef5d2a060c348da291e4e6a7d20b5253fe5a21
P4lten <altenbergerp@gmail.com>
2025-11-09 16:08:09 +0100

    added header code

---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import "./Header.css"
+
+function Header(){
+
+    return(
+        <>
+        <header>
+            <div>
+                Menu 1
+            </div>
+             <div>
+                Menu 2
+            </div>
+             <div>
+                Menu 3
+            </div>
+             <div>
+                Menu 4
+        
+            </div>
+        </header>
+        </>
+    )
+}
+
+export default Header
