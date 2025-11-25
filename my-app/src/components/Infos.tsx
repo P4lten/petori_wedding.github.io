8f0744ba2f64b5347dc00ec3233ac436d9efdbc5
P4lten <altenbergerp@gmail.com>
2025-11-25 15:29:45 +0100

    added pictures to the FAQ

--- my-app/src/components/Infos.tsx
+++ my-app/src/components/Infos.tsx
@@ -1,6 +1,9 @@
 import classes from "./Infos.module.css"
 import Accodion from "./Accordion"
 import whisky from "../../assets/images/whisky.jpeg"
+import trichterverbot from "../../assets/images/trichterverbot2.png"
+import pflanzenverbot from "../../assets/images/pflanzenverbot2.png"
+import prost from "../../assets/images/prost.png"
 
 function Infos() {
 
@@ -15,8 +18,8 @@ function Infos() {
                     <Accodion title="Beginn"
                         text="Auf der Einladung steht zwar das man schon um 11:30 kommen kann, das heißt aber nicht das ihr zu der Zeit auch kommen müsst.
                                 Das ist nur für die Leute die sich dort in ihrere Unterkunft zurechtmachen, offizieller start des Fesivals ist um 13:00Uhr!"
-                        imageSrc=""
-                        imgAlt="" />
+                        imageSrc={prost}
+                        imgAlt="zwei menschen beim anstoßen mit bier" />
 
                     <Accodion title="Unterkunft"
                         text="Wer noch keine Unterkunft hat hier eine Liste von unterkünften die es in der Nähe sind:
@@ -49,8 +52,8 @@ function Infos() {
                     <Accodion title="Geschenke"
                         text="Bitte keine Pflanzen, davon haben wir genug daheim!!!
                                 Über was wir uns immer freuen ist eine aufstockung unserer Urlaubskassa, da wir sehr gerne verreisen."
-                        imageSrc=""
-                        imgAlt="" />
+                        imageSrc={pflanzenverbot}
+                        imgAlt="pflanzenverbot schild" />
 
                     <Accodion title="Ablauf"
                         text="Siehe Time-Table im Menü oben"
@@ -59,8 +62,8 @@ function Infos() {
 
                     <Accodion title="Trichter"
                         text="Auch wenn wir ein Festival Thema haben, herrscht am ganzen Gelände Trichterverbot, da es dadurch sehr leicht zu Ausschreitungen kommen kann!"
-                        imageSrc=""
-                        imgAlt="" />
+                        imageSrc={trichterverbot}
+                        imgAlt="trichterverbotsschild" />
 
                     <Accodion title="Ticket Verkauf"
                         text=" Diese Tickets kann man nicht verkaufen, was glaubst du eigentlich?!"
